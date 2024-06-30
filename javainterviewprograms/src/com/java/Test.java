0e7af3a5dcec142a6e77d948337bb648cb4712f8
sivaparvathi-2024 <ssiva@SIVAPARVATHI>
2024-06-30 21:42:10 +0530

    program for making string revers

--- javainterviewprograms/src/com/java/Test.java
+++ javainterviewprograms/src/com/java/Test.java
@@ -2,6 +2,12 @@ package com.java;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("hellow rod");
+		String s1 = "mohan";
+		String rev = "";
+		for (int i = s1.length() - 1; i <= 0; i--) {
+			rev = rev + s1.charAt(i);
+		}
+		System.out.println(rev);
+
 	}
 }
